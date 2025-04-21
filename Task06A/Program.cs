9a2b6a7d1e712e2175795263b5b3f6df1cee759a
Sofia <sofiaozhgihina@yandex.ru>
2025-04-21 08:29:41 +0400

    Task06

---
+++ Task06A/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+double k1 = double.Parse(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
+double c;
+c= Math.Sqrt(k1*k1+k2*k2);
+Console.WriteLine(c);
---
+++ Task06B/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+double k1 = double.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());
+double k2;
+k2 =Math.Sqrt(c*c-k1*k1);
+Console.WriteLine(k2);
