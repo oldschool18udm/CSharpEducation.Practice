4a933648b0501a5b023d98f901e983157484c5f0
Sofia <sofiaozhgihina@yandex.ru>
2025-05-10 17:38:27 +0400

    Practoce03 Task 01, 02 - Students

---
+++ Practice03.Task01/Student.cs
@@ -0,0 +1,12 @@
+namespace Practice03.Task01;
+
+public class Student
+{
+    public  string name;
+    public   int age;
+
+    public  void PrintStudent()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
