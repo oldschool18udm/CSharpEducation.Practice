6240aef973e08e18d86c5d7e10f96fabc062f4cc
Sofia <sofiaozhgihina@yandex.ru>
2025-05-10 17:51:56 +0400

    Practice03 Task03 Calculator

---
+++ Practice03.Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Practice03.Task03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float a1=6;
+        float a2=5;
+        Calculator calc = new Calculator();
+        Console.WriteLine(calc.Add(a1, a2));
+        Console.WriteLine(calc.Sub(a1, a2));
+        Console.WriteLine(calc.Mult(a1, a2));
+        Console.WriteLine(calc.Div(a1, a2));
+    }
+}
