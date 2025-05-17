7674f1d6eb5922571cb7170655cf2950e21b0fcd
Sofia <sofiaozhgihina@yandex.ru>
2025-05-17 19:14:35 +0400

    Practice03 Task11

---
+++ Practice03.Task11/Book.cs
@@ -0,0 +1,8 @@
+namespace Practice03.Task11;
+
+public class Book
+{
+    public string Author;
+    public string Title;
+
+}
---
+++ Practice03.Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice03.Task11;
+
+class Program
+{
+    public static string BookInfo(Book book)
+    {
+        return $"{book.Title} - {book.Author}";
+    }
+    static void Main(string[] args)
+    {
+        Book book = new Book();
+        book.Author = "Gogol";
+        book.Title = "Viy";
+        Console.WriteLine(BookInfo(book));
+    }
+}
